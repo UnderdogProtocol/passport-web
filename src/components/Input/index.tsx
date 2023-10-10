a06f5765b34d2602b24fe55bb90ddbb8ab6e9562
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-10-11 00:31:38 +0530

    fix: components fix - copied from upstream
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import { useState, HTMLInputTypeAttribute, HTMLProps } from "react";
+import { useState, HTMLInputTypeAttribute, HTMLProps, forwardRef } from "react";
 
 import clsx from "clsx";
 import {
@@ -11,66 +11,78 @@ import { Label, LabelProps } from "../Label";
 
 export interface InputProps
   extends Omit<HTMLProps<HTMLInputElement>, "size">,
-    ErrorProps, LabelProps {
+    ErrorProps,
+    LabelProps {
   htmlType?: HTMLInputTypeAttribute;
   size?: TailwindSize;
 }
 
-export const Input: React.FC<InputProps> = ({
-  size = "md",
-  htmlType,
-  className,
-  maxLength,
-  error,
-  label,
-  help,
-  ...inputProps
-}) => {
-  const [currentLength, setCurrentLength] = useState(0);
-  const paddingClassName = sizeToPaddingClassName[size];
-  const fontSizeClassName = sizeToFontSizeClassName[size];
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      size = "md",
+      htmlType,
+      className,
+      maxLength,
+      error,
+      label,
+      help,
+      ...inputProps
+    },
+    ref
+  ) => {
+    console.log(inputProps);
+    const [currentLength, setCurrentLength] = useState(0);
+    const paddingClassName = sizeToPaddingClassName[size];
+    const fontSizeClassName = sizeToFontSizeClassName[size];
 
-  const inputClassName = clsx(
-    "focus:outline-none w-full placeholder-dark-accent bg-transparent",
-    paddingClassName,
-    className
-  );
+    const inputClassName = clsx(
+      "focus:outline-none w-full placeholder-dark-accent bg-transparent",
+      paddingClassName,
+      className
+    );
 
-  const containerClassName = clsx(
-    "flex items-center rounded-md overflow-hidden text-dark dark:text-light transition-colors border",
-    "bg-dark-light border-dark-accent focus-within:border-lightPurple-400",
-    fontSizeClassName
-  );
+    const containerClassName = clsx(
+      "flex items-center rounded-md overflow-hidden text-dark dark:text-light transition-colors border",
+      "bg-dark-light border-dark-accent focus-within:border-lightPurple-400",
+      fontSizeClassName
+    );
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrentLength(e.target.value.length);
-    if (inputProps.onChange) inputProps.onChange(e);
-  };
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setCurrentLength(e.target.value.length);
+      if (inputProps.onChange) inputProps.onChange(e);
+    };
 
-  return (
-    <div className={className}>
-      {label && <Label label={label} help={help} />}
+    return (
+      <div className={className}>
+        {label && <Label label={label} help={help} />}
 
-      <div className={clsx("w-full", inputProps.disabled && "opacity-50")}>
-        <div className="relative">
-          <div className={containerClassName}>
-            <input
-              {...inputProps}
-              maxLength={maxLength}
-              type={htmlType}
-              className={inputClassName}
-              style={{ WebkitAppearance: "none" }}
-              onChange={handleInputChange}
-            />
-            {maxLength && (
-              <div className="text-dark-accent absolute bottom-1 right-2 text-xs">
-                {`${currentLength}/${maxLength}`}
-              </div>
-            )}
+        <div className={clsx("w-full", inputProps.disabled && "opacity-50")}>
+          <div className="relative">
+            <div className={containerClassName}>
+              <input
+                {...inputProps}
+                ref={ref}
+                maxLength={maxLength}
+                type={htmlType}
+                className={inputClassName}
+                style={{ WebkitAppearance: "none" }}
+                onChange={handleInputChange}
+              />
+              {maxLength && (
+                <div className="text-dark-accent absolute bottom-1 right-2 text-xs">
+                  {`${currentLength}/${maxLength}`}
+                </div>
+              )}
+            </div>
           </div>
         </div>
+        <Error error={error} className="mt-2" />
       </div>
-      <Error error={error} className="mt-2" />
-    </div>
-  );
-};
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export { Input };
