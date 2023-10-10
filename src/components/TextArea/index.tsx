c5c3731660bcf3b332fef3977cced265949e9530
Kevin Arifin <kevarifin14@gmail.com>
2023-10-10 09:01:35 -0700

    input & text area components

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -1,4 +1,4 @@
-import { useState, HTMLProps } from "react";
+import { useState, HTMLProps, forwardRef } from "react";
 
 import clsx from "clsx";
 import {
@@ -18,59 +18,62 @@ export interface TextAreaProps
   error?: ErrorOption;
 }
 
-export const TextArea: React.FC<TextAreaProps> = ({
-  size = "md",
-  error,
-  className,
-  maxLength,
-  label,
-  help,
-  ...textAreaProps
-}) => {
-  const [currentLength, setCurrentLength] = useState(0);
-  const paddingClassName = sizeToPaddingClassName[size];
-  const fontSizeClassName = sizeToFontSizeClassName[size];
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  (
+    { size = "md", error, className, maxLength, label, help, ...textAreaProps },
+    ref
+  ) => {
+    const [currentLength, setCurrentLength] = useState(0);
+    const paddingClassName = sizeToPaddingClassName[size];
+    const fontSizeClassName = sizeToFontSizeClassName[size];
 
-  const textAreaClassName = clsx(
-    "focus:outline-none w-full placeholder-dark-accent bg-transparent",
-    paddingClassName,
-    className
-  );
+    const textAreaClassName = clsx(
+      "focus:outline-none w-full placeholder-dark-accent bg-transparent",
+      paddingClassName,
+      className
+    );
 
-  const containerClassName = clsx(
-    "flex items-center rounded-md overflow-hidden text-dark dark:text-light transition-colors border",
-    "bg-dark-light border-dark-accent focus-within:border-lightPurple-400",
-    fontSizeClassName
-  );
+    const containerClassName = clsx(
+      "flex items-center rounded-md overflow-hidden text-dark dark:text-light transition-colors border",
+      "bg-dark-light border-dark-accent focus-within:border-lightPurple-400",
+      fontSizeClassName
+    );
 
-  const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setCurrentLength(e.target.value.length);
-    if (textAreaProps.onChange) textAreaProps.onChange(e);
-  };
+    const handleTextAreaChange = (
+      e: React.ChangeEvent<HTMLTextAreaElement>
+    ) => {
+      setCurrentLength(e.target.value.length);
+      if (textAreaProps.onChange) textAreaProps.onChange(e);
+    };
 
-  return (
-    <div className={className}>
-      {label && <Label label={label} help={help} />}
-
-      <div className={clsx("w-full", textAreaProps.disabled && "opacity-50")}>
-        <div className="relative">
-          <div className={containerClassName}>
-            <textarea
-              {...textAreaProps}
-              maxLength={maxLength}
-              className={textAreaClassName}
-              style={{ WebkitAppearance: "none" }}
-              onChange={handleTextAreaChange}
-            />
-            {maxLength && (
-              <div className="text-dark-accent absolute bottom-1 right-2 text-xs">
-                {`${currentLength}/${maxLength}`}
-              </div>
-            )}
+    return (
+      <div className={className}>
+        {label && <Label label={label} help={help} />}
+        <div className={clsx("w-full", textAreaProps.disabled && "opacity-50")}>
+          <div className="relative">
+            <div className={containerClassName}>
+              <textarea
+                {...textAreaProps}
+                ref={ref} // Passing the ref to the textarea element
+                maxLength={maxLength}
+                className={textAreaClassName}
+                style={{ WebkitAppearance: "none" }}
+                onChange={handleTextAreaChange}
+              />
+              {maxLength && (
+                <div className="text-dark-accent absolute bottom-1 right-2 text-xs">
+                  {`${currentLength}/${maxLength}`}
+                </div>
+              )}
+            </div>
           </div>
         </div>
+        <Error error={error} className="mt-2" />
       </div>
-      <Error error={error} className="mt-2" />
-    </div>
-  );
-};
+    );
+  }
+);
+
+TextArea.displayName = "TextArea"; // Add this line
+
+export { TextArea };
