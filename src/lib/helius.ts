1c804122ce81bc193766d7dafb898224de4fe7a5
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-10-12 21:34:28 +0530

    refactor: helius env config
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/lib/helius.ts
+++ src/lib/helius.ts
@@ -1,8 +1,8 @@
 import { DAS, Helius } from "helius-sdk";
 
 export const helius = new Helius(
-  process.env.HELIUS_KEY!,
-  process.env.HELIUS_NETWORK==="devnet" ? "devnet" : "mainnet-beta",
+  process.env.NEXT_PUBLIC_HELIUS_KEY!,
+  process.env.NEXT_PUBLIC_HELIUS_NETWORK==="devnet" ? "devnet" : "mainnet-beta",
 );
 
 export const searchAssets = async (
--- src/pages/[namespace]/[mintAddress].tsx
+++ src/pages/[namespace]/[mintAddress].tsx
@@ -1,8 +1,20 @@
 import { getLayout } from "@/components/AppLayout";
+import { AssetView } from "@/views/AssetView";
 import { MailAssetView } from "@/views/MailAssetView";
+import { useRouter } from "next/router";
 
 export default function AssetPage() {
-  return <MailAssetView />;
+
+  const router = useRouter();
+console.log("NAMESPACE");
+console.log(router.query.namespace);
+
+  switch (router.query.namespace) {
+    case "mail":
+      return <MailAssetView />;
+    default:
+      return <AssetView />;
+  }
 }
 
-AssetPage.getLayout= getLayout;
+AssetPage.getLayout = getLayout;
