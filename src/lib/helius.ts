4278424758d5da8cd21dd024f8de454444f8ad31
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-10-12 21:25:13 +0530

    refactor: helius env config
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/lib/helius.ts
+++ src/lib/helius.ts
@@ -1,9 +1,8 @@
 import { DAS, Helius } from "helius-sdk";
 
 export const helius = new Helius(
-  "f961a77f-7072-4316-b9a2-1cab32a4d4a1",
-  // process.env.NEXT_PUBLIC_NETWORK === "DEVNET" ? "devnet" : "mainnet-beta"
-  "devnet"
+  process.env.HELIUS_KEY!,
+  process.env.HELIUS_NETWORK==="devnet" ? "devnet" : "mainnet-beta",
 );
 
 export const searchAssets = async (
