fc476e5a9a85feb508fbfd5a9204481370cb66e6
Kevin Arifin <kevarifin14@gmail.com>
2023-10-20 13:54:57 -0700

    add context to domain form

--- src/views/CreateDomainView/index.tsx
+++ src/views/CreateDomainView/index.tsx
@@ -1,17 +1,30 @@
 import { Container } from "@/components/Container";
 import { CreateDomainForm } from "./CreateDomainForm";
 import { MediaObject } from "@/components/MediaObject";
+import { Button } from "@/components/Button";
 
 export const CreateDomainView: React.FC = () => {
   return (
     <Container size="2xl" className="py-16 space-y-8">
-      <MediaObject 
-        title="Create Domain" 
-        description="Owning a domain allows you to activate Passports within your domain"
-        size="2xl" 
-      />
+      <div className="space-y-2">
+        <MediaObject
+          title="Create Passport Domain"
+          description={`For a limited time, we are offering early partners the opportunity for lifetime ownership over a Passport Domain (normally 10 USDC / month). By owning a Passport Domain, you can activate Passports within your domain in your own application.`}
+          size="2xl"
+        />
+
+        <Button
+          type="link"
+          className="text-primary p-0"
+          onClick={() =>
+            window.open("https://underdog.readme.io/reference/passport")
+          }
+        >
+          Learn More about Passport
+        </Button>
+      </div>
 
       <CreateDomainForm />
     </Container>
   );
-}
+};
