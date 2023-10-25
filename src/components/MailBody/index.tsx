fd5f91e7fd017cecd575d976e15a9fd14e3e4156
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-10-25 23:18:42 +0530

    refactor: truncate extra text
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/components/MailBody/index.tsx
+++ src/components/MailBody/index.tsx
@@ -18,7 +18,7 @@ const MailBody: React.FC<MailBodyProps> = ({ description, timestamp,borderColor 
             <div className="text-right text-gray-500 p-2">
                 {timestamp}
             </div>
-            <div className={`border ${isExpanded ? 'h-auto' : 'h-16'} overflow-hidden transition-height duration-300 ${borderColor?borderColor:''}`}>
+            <div className={`border ${isExpanded ? 'h-auto' : 'h-16 truncate'} overflow-ellipsis transition-height duration-300 ${borderColor?borderColor:''}`}>
                 <div className="text-2xl text-white break-words p-4" onClick={toggleExpansion}>
                     {description}
                 </div>
