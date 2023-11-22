86eb22f04ac568e7ca11569da85af9c16d6571c3
Kevin Arifin <kevarifin14@gmail.com>
2023-11-21 16:03:14 -0800

    update sign in page with brief description

--- src/views/SignInView/index.tsx
+++ src/views/SignInView/index.tsx
@@ -3,13 +3,17 @@ import { Card } from "@/components/Card";
 // import { SignInWithWalletButton } from "@/components/SignInWithWalletButton";
 import { Container } from "@/components/Container";
 import { Logo } from "@/components/Logo";
+import { Header } from "@/components/MediaObject/Header";
 import { signIn } from "next-auth/react";
 
 export const SignInView: React.FC = () => {
   return (
     <Container size="xl" className="h-screen flex items-center justify-center">
       <Card className="p-12 space-y-8 w-full">
-        <Logo full dark className="flex justify-center" />
+        <div className="flex justify-center flex-col space-y-4">
+          <Logo full dark className="flex justify-center" />
+          <Header title="Collect digital assets without a web3 wallet" size="xl" className="mx-auto" />
+        </div>
 
         <div className="space-y-2">
           <div className="grid grid-cols-3 gap-2">
